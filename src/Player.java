c41170f02f000eb89b2883483102c8c3c6a60d35
Vincent Gu <vincent.gu@gmail.com>
2022-07-26 20:47:10 -0600

    added some stuff to Player

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,41 @@
 public class Player {
+    private final String name;
+    private int index=0;
+    private int money = 1500;
+    private boolean inJail = false;
+
+    public boolean payMoney(int amount){
+        if(money >= amount) {
+            this.money -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void gainMoney(int amount){ this.money += amount; }
+
+    public void move(int nTiles, Board board){
+        //implement
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public boolean isInJail() {
+        return inJail;
+    }
 }
